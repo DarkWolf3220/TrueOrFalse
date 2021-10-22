81a65f354d8fe2c540c5f2ed08f57d5f77ced09b
DarkWolf3220 <milosz.pietka32@gmail.com>
2021-10-23 00:56:06 +0200

    Summary

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ function red(element, color){
 }
 
 function check(){
-    var check = Math.floor(Math.random()*100)+1
+    var check = Math.floor(Math.random()*99)+1
 
-    if(check > 50){
+    console.log(check);
+
+    if(check < 33){
         document.getElementById("result").style.color = "green";
         document.getElementById("result").innerHTML = "TAK!";
+        }
+    else if(check < 66){
+        document.getElementById("result").style.color = "yellow";
+        document.getElementById("result").innerHTML = "MOŻE";
     }else{
         document.getElementById("result").style.color = "red";
         document.getElementById("result").innerHTML = "NIE!";
